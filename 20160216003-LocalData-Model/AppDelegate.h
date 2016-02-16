7b81f5056375c8d050a1cc31d38be625e92a7c4f
raineryue <jinlei_yue@sina.com>
2016-02-16 15:23:21 +0800

    Initial Commit

---
+++ 20160216003-LocalData-Model/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  20160216003-LocalData-Model
+//
+//  Created by Rainer on 16/2/16.
+//  Copyright © 2016年 Rainer. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 20160216003-LocalData-Model/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  20160216003-LocalData-Model
+//
+//  Created by Rainer on 16/2/16.
+//  Copyright © 2016年 Rainer. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
